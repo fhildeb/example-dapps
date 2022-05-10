a5e417b0365a37c47925a5fb696a0bcac7866ed6
fhildeb <felix@lukso.io>
2022-05-10 15:39:46 +0200

    updated & simplified connection flow

--- utils.js
+++ utils.js
@@ -1,70 +1,102 @@
 async function connectWeb3() {
-  let accoutnsArray = [];
+
   // Check if Browser is Chrome or Firefox
   if (
     navigator.userAgent.indexOf("Firefox") != -1 ||
     navigator.userAgent.indexOf("Chrome") != -1
   ) {
-    // Check if browser extension is installed and logged in (address is available)
+    
+    // Check if browser extension is installed
     try {
-      // Get accounts
-      if (window.ethereum) {
-        // Enable Login button functionality
-        el("#loginbutton").addEventListener("click", async function () {
-          await ethereum.request({ method: "eth_requestAccounts" });
-          el("#login").style.display = "none";
-          // Rerun dApp after login
-          await dApp();
-        });
 
-        // Request accounts
-        try {
-          accoutnsArray = await ethereum.request({ method: "eth_accounts" });
-        } catch (e) {
-          // Universal Profile Extension detected and locked
-          if (e.code == 4100) {
-            // Login to be able to request accounts
-            el("#login").style.display = "block";
-            return false;
-          }
+      // Get account
+      if (window.ethereum) {
+
+        // Request account
+        let account = await ethereum.request({ method: "eth_accounts" });
+
+        // If no account was found
+        if (!account.length) {
+          
+          // Enable login button functionality
+          el("#loginbutton").addEventListener("click", async function () {
+
+            // Request login of extension
+            await ethereum.request({ method: "eth_requestAccounts" });
+
+            // Hide button after login was successful
+            el("#login").style.display = "none";
+
+            // Rerun dApp with logged-in extension
+            await dApp();
+          });
+
+          // Show login button 
+          el("#login").style.display = "block";
+          return false;
+        } 
+        else {
+          return true;
         }
       }
-
-      // Login if no accounts were found
-      if (!accoutnsArray.length) {
-        el("#login").style.display = "block";
-        return false;
-      } else {
-        return true;
-      }
+    
+    // Provider not set or invalid
     } catch (e) {
-      // Provider not set or invalid
+
+      // Show install extension notice
       el("#install").style.display = "block";
       return false;
     }
   }
   // Browser is not supported
   else {
+
+    // Show unsupported browser notice
     el("#browser").style.display = "block";
     return false;
   }
 }
 
 async function checkNetwork() {
-  // Access web3
-  const web3 = new Web3(window.ethereum);
+  try{
 
-  // If its MetaMask, check if network is the right one
-  if (window.ethereum.isMetaMask) {
-    const networkID = await web3.eth.net.getId();
-    const networkHost = await web3.eth.net._provider.host;
+    // Access web3
+    const web3 = new Web3(window.ethereum);
 
-    // Check if the right network is configured
-    if (networkID !== 22 && networkHost !== "https://rpc.l14.lukso.network") {
-      el("#network").style.display = "block";
-      return false;
+    // Get account
+    let account = await ethereum.request({ method: "eth_accounts" });
+
+    // Get the bytecode of the address or smart contract
+    let address = await web3.eth.getCode(account[0]);
+
+    // If address is EOA, likely a 3rd party extension is used
+    if (address == '0x') {
+
+      // Get its network ID
+      const networkID = await web3.eth.net.getId();
+
+      // Check if its connected to the wrong network
+      if (networkID !== 22) {
+        
+        // Show wrong network notice
+        el("#network").style.display = "block";
+        return false;
+      }
+
+      // 3rd party extension is connected to the right network
+      return true;
     }
+
+    // Likely installed the UP extension
     return true;
   }
-  return true;
+  catch(e){
+
+    /**
+     *  Extension not installed or locked:
+     *  connectWeb3() needs to be run before
+     */ 
+    return false;
+  }
+  
 }
