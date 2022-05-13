c287ee24ee40824c292948f8b3e7664b21a7a290
fhildeb <felix@lukso.io>
2022-05-13 20:31:27 +0200

    add jQuery to outsource notes and warnings

--- events.js
+++ events.js
@@ -1,24 +1,28 @@
 // Helper function to search for HTML elements
 let el = function (id) { return document.querySelector(id); };
 
-// If Web3 extension found
-if (window.ethereum) {
+// Before adding events to HTML elements, load them into the Webpage
+$('#notes').load('notes.html', function() {
 
-  // Enable login button functionality
-  el("#loginbutton").addEventListener("click", async function () {
-  
-    // Request login of extension
-    await ethereum.request({ method: "eth_requestAccounts" });
-  
-    // Hide button after login was successful
-    el("#login").style.display = "none";
-  
-    // Restart application with logged-in extension
-    await app();
-  });
+  // If Web3 extension found
+  if (window.ethereum) {
 
-  // Enable network swap
-    el("#swapnetwork").addEventListener("click", async function () {
-    await addLuksoTestnet();
-  });
-}
+    // Enable login button functionality
+    el("#loginbutton").addEventListener("click", async function () {
+    
+      // Request login of extension
+      await ethereum.request({ method: "eth_requestAccounts" });
+    
+      // Hide button after login was successful
+      el("#login").style.display = "none";
+    
+      // Restart application with logged-in extension
+      await app();
+    });
+
+    // Enable network swap
+      el("#swapnetwork").addEventListener("click", async function () {
+      await addLuksoTestnet();
+    });
+  }
+});
