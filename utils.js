889f183a615c4d6c13b926af8f7cbe8afc757664
fhildeb <felix@lukso.io>
2022-05-20 11:52:42 +0200

    update links and minor styling changes

--- utils.js
+++ utils.js
@@ -1,48 +1,48 @@
 async function connectWeb3() {
 
-  // Check if Browser is Chrome or Firefox
-  if (
-    navigator.userAgent.indexOf("Firefox") != -1 ||
-    navigator.userAgent.indexOf("Chrome") != -1
-  ) {
-    
-    // Check if browser extension is installed
-    try {
-
-      // Get account
-      if (window.ethereum) {
-
-        // Request account
-        let account = await ethereum.request({ method: "eth_accounts" });
-
-        // If no account was found
-        if (!account.length) {
-
-          // Show login button 
-          el("#login").style.display = "block";
-          return false;
-        } 
-        else {
-          return true;
+    // Check if Browser is Chrome or Firefox
+    if (
+      navigator.userAgent.indexOf("Firefox") != -1 ||
+      navigator.userAgent.indexOf("Chrome") != -1
+    ) {
+      
+      // Check if browser extension is installed
+      try {
+  
+        // Get account
+        if (window.ethereum) {
+  
+          // Request account
+          let account = await ethereum.request({ method: "eth_accounts" });
+  
+          // If no account was found
+          if (!account.length) {
+  
+            // Show login button 
+            el("#login").style.display = "block";
+            return false;
+          } 
+          else {
+            return true;
+          }
         }
+      
+      // Provider not set or invalid
+      } catch (e) {
+  
+        // Show install extension notification
+        el("#install").style.display = "block";
+        return false;
       }
+    }
     
-    // Provider not set or invalid
-    } catch (e) {
-
-      // Show install extension notification
-      el("#install").style.display = "block";
+    // Browser is not supported
+    else {
+  
+      // Show unsupported browser notification
+      el("#browser").style.display = "block";
       return false;
     }
-  }
-
-  // Browser is not supported
-  else {
-
-    // Show unsupported browser notification
-    el("#browser").style.display = "block";
-    return false;
-  }
 }
 
 async function checkMinimalBalance() {
@@ -61,78 +61,78 @@ async function checkMinimalBalance() {
 }
 
 async function checkNetwork() {
-  try{
-
-    // Access web3
-    const web3 = new Web3(window.ethereum);
-
-    // Get account
-    let account = await ethereum.request({ method: "eth_accounts" });
-
-    // Get the bytecode of the address or smart contract
-    let bytecode = await web3.eth.getCode(account[0]);
-
-    // If address is EOA, likely a 3rd party extension is used
-    if (bytecode === '0x') {
-
-      // Show 3rd party extension notification
-      el("#extension").style.display = "block";
-
-      // Get its network ID
-      const networkID = await web3.eth.net.getId();
-
-      // Check if its connected to the wrong network
-      if (networkID !== 22) {
+    try{
+  
+      // Access web3
+      const web3 = new Web3(window.ethereum);
+  
+      // Get account
+      let account = await ethereum.request({ method: "eth_accounts" });
+  
+      // Get the bytecode of the address or smart contract
+      let bytecode = await web3.eth.getCode(account[0]);
+  
+      // If address is EOA, likely a 3rd party extension is used
+      if (bytecode === '0x') {
+  
+        // Show 3rd party extension notification
+        el("#extension").style.display = "block";
+  
+        // Get its network ID
+        const networkID = await web3.eth.net.getId();
+  
+        // Check if its connected to the wrong network
+        if (networkID !== 22) {
+          
+          // Show wrong network notification
+          el("#network").style.display = "block";
+          return false;
+  
+        }
         
-        // Show wrong network notification
-        el("#network").style.display = "block";
-        return false;
-
+        /**
+         * 3rd party extension is connected to the right network.
+         * Check if balance on network is enough to send transactions
+         */
+        await checkMinimalBalance();
+        return true;
       }
-      
-      /**
-       * 3rd party extension is connected to the right network.
-       * Check if balance on network is enough to send transactions
-       */
-      await checkMinimalBalance();
+  
+      // Likely installed the UP extension
       return true;
     }
-
-    // Likely installed the UP extension
-    return true;
-  }
-  catch(e){
-
-    /**
-     *  Extension not installed or locked:
-     *  connectWeb3() needs to be run before
-     */ 
-    return false;
-  }
+    catch(e){
+  
+      /**
+       *  Extension not installed or locked:
+       *  connectWeb3() needs to be run before
+       */ 
+      return false;
+    }
 }
 
 async function addLuksoTestnet() {
-  try {
-
-    // Open request to add custom network
-    await window.ethereum.request({
-      method: 'wallet_addEthereumChain',
-      params: [
-        {
-          chainId: '0x16',
-          chainName: 'LUKSO L14',
-          nativeCurrency: {
-            name: 'LUKSO',
-            symbol: 'LYXt',
-            decimals: 18,
+    try {
+  
+      // Open request to add custom network
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x16',
+            chainName: 'LUKSO L14',
+            nativeCurrency: {
+              name: 'LUKSO',
+              symbol: 'LYXt',
+              decimals: 18,
+            },
+            rpcUrls: ['https://rpc.l14.lukso.network'],
+            blockExplorerUrls: ['https://blockscout.com/lukso/l14'],
           },
-          rpcUrls: ['https://rpc.l14.lukso.network'],
-          blockExplorerUrls: ['https://blockscout.com/lukso/l14'],
-        },
-      ],
-    });
-  } catch (err) {
-      
-    // User denied access
-  }
+        ],
+      });
+    } catch (err) {
+        
+      // User denied access
+    }
 }
